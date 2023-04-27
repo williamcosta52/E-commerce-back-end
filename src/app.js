580f85c054c7519df195ff9f9daa8cde7db65ade
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-04-27 14:27:38 -0300

    Feat: adicionada funcionadlidade de cadastro

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import joi from "joi"
+import bcrypt from "bcrypt"
 
 //Criação do servidor
 const app = express()
@@ -20,6 +22,61 @@ try {
     console.log(err.message)
 }
 const db = mongoClient.db()
+
+
+//Endpoints 
+
+app.post("/singup", async (req, res) => {
+    const {name, email, password, age} = req.body
+    const singUpSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required().min(3),
+        age: joi.number().required()
+    })
+
+    const validation = singUpSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+    const hash = bcrypt.hashSync(password, 10)
+    const newUser = {name, email, password: hash, age}
+    try {
+        const emailUsed = await db.collection("users").findOne({email})
+        if (emailUsed) return res.status(409).send("Esse email já está cadastrado")
+        await db.collection("users").insertOne(newUser)
+        return res.sendStatus(201)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.get("/singup", async (req, res) => {
+    try {
+        const users = await db.collection("users").find().toArray()
+        return res.status(200).send(users)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.delete("/singup/:email", async (req, res) => {
+const {email} = req.params
+try{
+    const deleted = await db.collection("users").deleteOne({email})
+    if (deleted.deletedCount === 0) return res.status(404).send("Esse item não existe!")
+    return res.status(200).send(`Participante com email ${email} deletado com sucesso!`)
+}
+catch(err){
+    res.status(500).send(err.message)
+}
+})
+
+
+
 
 //Deixar o app escutante
 const PORT = 5000

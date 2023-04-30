f2da37780cf05a09a38d604f420bae9866cd65c8
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-04-30 01:05:24 -0300

    Feat: funcionalidade do carrinho por token

--- src/app.js
+++ src/app.js
@@ -30,8 +30,9 @@ app.post("/signup", async (req, res) => { //POSTAR CADASTRO
 		name: joi.required(),
 		email: joi.required(),
 		password: joi.required(),
-		age: joi.number().required(),
+		age: joi.required(),
 	});
+
 	const validation = singUpSchema.validate(req.body, { abortEarly: false });
 	if (validation.error) {
 		const errors = validation.error.details.map((detail) => detail.message);
@@ -44,37 +45,11 @@ app.post("/signup", async (req, res) => { //POSTAR CADASTRO
 		const emailUsed = await db.collection("users").findOne({ email });
 		if (emailUsed) return res.status(409).send("Esse email já está cadastrado");
 		await db.collection("users").insertOne(newUser);
-		return res.sendStatus(201);
+		return res.status(201).send(`Cadastro de ${newUser.name} realizado com sucesso!`);
 	} catch (err) {
 		res.status(500).send(err.message);
 	}
 });
-
-app.get("/signup", async (req, res) => { //VE OS CADASTRADOS
-	try {
-		const users = await db.collection("users").find().toArray();
-		return res.status(200).send(users);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
-});
-
-
-app.delete("/signup/:email", async (req, res) => { //DELETA CADASTRADO POR EMAIL
-	const { email } = req.params;
-	try {
-		const deleted = await db.collection("users").deleteOne({ email });
-		if (deleted.deletedCount === 0)
-			return res.status(404).send("Esse item não existe!");
-		return res.status(200).send(`Participante com email ${email} deletado com sucesso!`);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
-});
-
-
-
-
 
 app.post("/login", async (req, res) => { //FAZ O LOGIN
 	const { email, password } = req.body;
@@ -91,18 +66,6 @@ app.post("/login", async (req, res) => { //FAZ O LOGIN
 		res.status(500).send(err.message);
 	}
 });
-
-
-app.get("/login", async (req, res) => { //MOSTRA OS USUARIOS LOGADOS
-	try {
-		const usersLogins = await db.collection("sessions").find().toArray();
-		return res.status(200).send(usersLogins);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
-});
-
-
 
 app.post("/van/stock", async (req, res) => {  //ADMIN POSTAR ITEM AO ESTOQUE
 	const itemSchema = joi.object({
@@ -148,19 +111,6 @@ app.get("/van/stock", async (req, res) => {  //MOSTRA TODOS OS ITENS
 	}
 });
 
-app.delete("/van/stock/:name", async (req, res) => {  //RETIRA ITEM DO ESTOQUE POR NOME
-	const { name } = req.params;
-	try {
-		const deleted = await db.collection("stock").deleteOne({ name });
-		if (deleted.deletedCount === 0)
-			return res.status(404).send("Esse item não existe!");
-		return res.status(200).send(`Item ${name} retirado do estoque!`);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
-});
-
-
 
 app.get("/categories", async (req, res) => { //PEGA TODAS AS CATEGORIAS DO ESTOQUE
 	try {
@@ -171,22 +121,8 @@ app.get("/categories", async (req, res) => { //PEGA TODAS AS CATEGORIAS DO ESTOQ
 	}
 });
 
-app.delete("/categories/:category", async (req, res) => { //APAGA CATEGORIA E OS ITENS NELA
-	const { category } = req.params;
-	try {
-		const deletedCategory = await db.collection("categories").deleteOne({ category });
-		if (deletedCategory.deletedCount === 0)
-			return res.status(404).send("Essa categoria não existe!");
-        const deletedItemsInCategory = await db.collection("stock").deleteMany({category: category})
-		return res.status(200).send(`Categoria ${category} apagada, assim como todos os
-        ${deletedItemsInCategory.deletedCount} itens presentes nela!`);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
-});
 
-
-app.get("/:category", async (req, res) => {  //PEGA OS ITEMS DE UMA CATEGORIA
+app.get("/categoria/:category", async (req, res) => {  //PEGA OS ITEMS DE UMA CATEGORIA
 	const { category } = req.params;
 	try {
 		const categoryItems = await db.collection("stock").find({ category: category }).toArray();
@@ -208,12 +144,25 @@ app.get("/:category/:item", async (req, res) => {  //PEGA ITEM EM ESPECÍFICO
 });
 
 
-app.post("/home/cart", async (req, res) => {
-const itemAdded = req.body
+app.post("/home/cart", async (req, res) => {  //ADICIONA ITENS AO CARRINHO DO USUARIO DE ACORDO COM O TOKEN
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '')
+
+const {name, category, price, quantity, image } = req.body
+
     try{
-await db.collection("userCart").insertOne(itemAdded)
-const cart = await db.collection("userCart").find().toArray()
-return res.status(200).send(cart)
+    const thisUser = await db.collection("sessions").findOne({token})
+	const userCart = {
+		idUser: thisUser.idUser,
+		name, 
+		category, 
+		price, 
+		quantity, 
+		image
+	}
+await db.collection("userCart").insertOne(userCart)
+return res.status(200).send(`Item ${name} adicionado ao carrinho de ${thisUser.name}!`)
     }
     catch (err) {
 		res.status(500).send(err.message);
@@ -221,15 +170,87 @@ return res.status(200).send(cart)
 })
 
 
+app.get("/home/cart/show", async (req, res) => { //LISTA OS ITENS DO CARRINHO DO USUARIO DE ACOROD COM O TOKEN
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '')
 
-app.get("/home/cart/show", async (req, res) => {
     try{
-        const cart = await db.collection("userCart").find().toArray()
-        return res.status(200).send(cart)
+        const thisUser = await db.collection("sessions").findOne({token})
+        const cartShow = await db.collection("userCart").find({idUser: thisUser.idUser}).toArray()
+        return res.status(200).send(cartShow)
     }catch (err) {
 		res.status(500).send(err.message);
 	}
 })
+
+
+
+
+
+//PARA USO ADMINISTRATIVO
+
+app.get("/signup", async (req, res) => { //VE OS CADASTRADOS
+	
+	try {
+		const users = await db.collection("users").find().toArray();
+		return res.status(200).send(users);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+
+
+app.delete("/signup/:email", async (req, res) => { //DELETA CADASTRADO POR EMAIL
+	const { email } = req.params;
+	try {
+		const deleted = await db.collection("users").deleteOne({ email });
+		if (deleted.deletedCount === 0)
+			return res.status(404).send("Esse item não existe!");
+		return res.status(200).send(`Participante com email ${email} deletado com sucesso!`);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+
+app.get("/login", async (req, res) => { //MOSTRA OS USUARIOS LOGADOS
+	try {
+		const usersLogins = await db.collection("sessions").find().toArray();
+		return res.status(200).send(usersLogins);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+
+app.delete("/van/stock/:name", async (req, res) => {  //RETIRA ITEM DO ESTOQUE POR NOME
+	const { name } = req.params;
+	try {
+		const deleted = await db.collection("stock").deleteOne({ name });
+		if (deleted.deletedCount === 0)
+			return res.status(404).send("Esse item não existe!");
+		return res.status(200).send(`Item ${name} retirado do estoque!`);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
+
+app.delete("/categories/:category", async (req, res) => { //APAGA CATEGORIA E OS ITENS NELA
+	const { category } = req.params;
+	try {
+		const deletedCategory = await db.collection("categories").deleteOne({ category });
+		if (deletedCategory.deletedCount === 0)
+			return res.status(404).send("Essa categoria não existe!");
+        const deletedItemsInCategory = await db.collection("stock").deleteMany({category: category})
+		return res.status(200).send(`Categoria ${category} apagada, assim como todos os
+        ${deletedItemsInCategory.deletedCount} itens presentes nela!`);
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
 
 app.delete("/home/cart/show/:name", async (req, res) => {
     const { name } = req.params;
